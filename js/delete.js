5935799d2210e15d73140b84af2e076a18a8dd4a
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-12-19 02:30:01 +0100

    Complete /delete/

--- js/delete.js
+++ js/delete.js
@@ -3,12 +3,11 @@ $('#delete-form').submit((e) => {
 
   $.ajax({
     type: "DELETE",
-    url: 'https://api.tempfiles.download/delete/' + $.param({'id': $('#id').val(), 'p': $('#password').val(), 'delete': $('#del-pass').val()}),
+    url: 'https://api.tempfiles.download/delete/?' + $.param({'id': $('#id').val(), 'p': $('#password').val(), 'delete': $('#del-pass').val()}),
     success: (data, textStatus, xhr) => {
-      console.log(data);
-      console.log(xhr);
       $('#delete-form').hide();
-      $('#title').text("File uploaded");
+      $('#success').show();
+      $('#title').text("File Deleted");
     }
   });
 });
